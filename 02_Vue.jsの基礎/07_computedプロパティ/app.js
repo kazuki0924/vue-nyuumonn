1434ea2d16c3762d183d1ef8adc0b3ba7d429534
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-21 18:15:24 +0900

    10

--- 02_Vue.jsの基礎/07_computedプロパティ/app.js
+++ 02_Vue.jsの基礎/07_computedプロパティ/app.js
@@ -2,13 +2,26 @@ const vm = new Vue({
   el: '#app',
   data() {
     return {
-      sum: 1000
+      button_disabled: false
     }
   },
-  filters: {
-    numberWithDelimiter(value) {
-      if (!value) return '0'
-      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+  computed: {
+    button_label() {
+      console.log('button_label called')
+      return this.button_disabled ? '無効' : '有効'
+    },
+    now() {
+      return new Date().toTimeString()
+    }
+  },
+  methods: {
+    log() {
+      console.log(this.now)
     }
   }
 })
+
+/**
+ * vm.button_disabled = false
+ * -> button_label called
+ */
