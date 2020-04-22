d6980c0b2279f5e9b332b0d1a8fdbdfbdbff5c1b
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-22 23:38:07 +0900

    24

---
+++ 02_Vue.jsの基礎/23_スロット/app.js
@@ -0,0 +1,22 @@
+const Layout = {
+  template: `
+    <div class="container">
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  `
+}
+
+const vm = new Vue({
+  el: '#app',
+  components: {
+    layout: Layout
+  }
+})
